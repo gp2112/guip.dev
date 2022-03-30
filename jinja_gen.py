ff4c833d4064a721c844d961a807b3bec7f5d456
gp2112 <guircp@usp.br>
2022-03-30 09:57:33 -0300

    add blog.html

--- jinja_gen.py
+++ jinja_gen.py
@@ -1,8 +1,12 @@
 from jinja2 import Template, Environment, FileSystemLoader
+from bs4 import BeautifulSoup
+import os
 
 file_loader = FileSystemLoader('template')
 env = Environment(loader=file_loader)
 
+
+# render static pages
 pages = ('projetos', 'contato', 'index')
 
 for page in pages:
@@ -10,8 +14,34 @@ for page in pages:
     with open(name, 'w') as f:
         template = env.get_template(name)
         f.write(template.render())
+
 file_loader = FileSystemLoader('articles')
 env = Environment(loader=file_loader)
-with open('article.html', 'w') as f:
-    template = env.get_template('internet-nos-anos-2020-1.html')
-    f.write(template.render())
+
+# render blog articles
+articles = os.listdir('./articles')
+for article_file in articles:
+    with open('./blog/'+article_file, 'w') as f:
+        template = env.get_template(article_file)
+        f.write(template.render())
+
+# render blog page
+
+file_loader = FileSystemLoader('template')
+env = Environment(loader=file_loader)
+articles_data = []
+for article_file in articles:
+    with open(f'./blog/{article_file}') as f:
+        html = f.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find('title').get_text()
+    preview = soup.find('p').get_text()[:100]+'...'
+    articles_data.append({
+                'title':title,
+                'preview':preview,
+                'url':article_file
+            })
+with open('blog.html', 'w') as f:
+    template = env.get_template('blog.html')
+    f.write(template.render(articles=articles_data))
+
